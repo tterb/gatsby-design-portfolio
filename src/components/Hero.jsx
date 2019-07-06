ab9a3ea900a802ccb399081538ce8d983d1569b7
Brett Stevenson <therealbrettstevenson@gmail.com>
2019-07-05 19:36:47 -0700

    :iphone: Improve site responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 import tw from 'tailwind.macro'
 
 const Hero = styled.section`
-  ${tw`relative -mt-16 overflow-hidden`}
+  ${tw`relative xs:mt-0 md:-mt-16 overflow-hidden`}
   height: ${props => (props.single ? '28vw' : '40vw')};
   margin-bottom: ${props => (props.single ? '0' : '1rem')};
   @media (max-width: ${props => props.theme.breakpoints.m}), (max-device-width: ${props => props.theme.breakpoints.m}) {
@@ -15,6 +15,9 @@ const Hero = styled.section`
     (max-device-width: ${props => props.theme.breakpoints.xs}) {
     height: 400px;
     margin-top: -7rem;
+  }
+  @media screen and (max-width: 420px) {
+    height: 40vw;
   }
 `
 
